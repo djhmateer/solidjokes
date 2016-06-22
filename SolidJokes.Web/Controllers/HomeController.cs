6dbb9516ccc26635c78282f4e1f60f3d8d844a44
Dave Mateer <davemateer@gmail.com>
2016-06-22 08:01:42 +0100

    about page in UK date time format

--- SolidJokes.Web/Controllers/HomeController.cs
+++ SolidJokes.Web/Controllers/HomeController.cs
@@ -1,39 +1,11 @@
-﻿using System;
-using SolidJokes.Core.Models;
+﻿using SolidJokes.Core.Models;
 using SolidJokes.Core.Services;
 using System.Collections.Generic;
-using System.IO;
+using System.Globalization;
 using System.Reflection;
 using System.Web.Mvc;
 
 namespace SolidJokes.Web.Controllers {
-    public static class Helpers
-    {
-        //http://stackoverflow.com/questions/1600962/displaying-the-build-date
-        public static DateTime GetLinkerTime(this Assembly assembly, TimeZoneInfo target = null)
-        {
-            var filePath = assembly.Location;
-            const int c_PeHeaderOffset = 60;
-            const int c_LinkerTimestampOffset = 8;
-
-            var buffer = new byte[2048];
-
-            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                stream.Read(buffer, 0, 2048);
-
-            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
-            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
-            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
-
-            var tz = target ?? TimeZoneInfo.Local;
-            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
-
-            return localTime;
-        }
-    }
-
     public class HomeController : Controller, IHomeController {
         private readonly IJokeViewer viewer;
         private readonly IJokeVoter voter;
@@ -84,7 +56,8 @@ namespace SolidJokes.Web.Controllers {
         public ActionResult About() {
             // build done on west coast USA (Appveyor)
             // Server is in Ireland so want it to display UK Time.
-            ViewBag.DateOfCompile = Assembly.GetExecutingAssembly().GetLinkerTime().ToLocalTime();
+            ViewBag.DateOfCompile = Assembly.GetExecutingAssembly().GetLinkerTime()
+                .ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
             return View();
         }
     }
